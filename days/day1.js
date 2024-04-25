41c8076623474075d4c7fb56a733434f572bd443
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2024-04-25 13:29:08 -0600

    Day 1 part 2

---
+++ days/day1.js
@@ -0,0 +1,43 @@
+// @ts-check
+const u = require('../utils');
+const input = require("../inputs/day1.json")
+/**
+ * Gets the first and last digit and returns that as a number
+ * @param {string} input 
+ */
+function calibrateConvert(input) {
+    const str = input.replace(/[^0-9]/g, "");
+    return parseInt(str[0] + str[str.length - 1]);
+}
+
+const numbers = [
+    "one", "two", "three",
+    "four", "five", "six",
+    "seven", "eight", "nine"
+]
+
+/**
+ * Takes text numbers into account
+ * @param {string} input
+*/
+function includeText(input) {
+    const nums = [];
+    for (let i = 0; i < input.length; i++) {
+        const substring = input.substring(i);
+        const num = parseInt(substring.charAt(0)) || numbers.findIndex(n => substring.startsWith(n)) + 1
+        if (num > 0) nums.push(num);
+    }
+    return calibrateConvert(nums.join(""))
+}
+
+// part 1
+u.time(() => {
+    const results = input.map(i => calibrateConvert(i))
+    return results.reduce((p, c) => p + c, 0);
+})
+
+// part 2
+u.time(() => {
+    const results = input.map(i => includeText(i))
+    return results.reduce((p, c) => p + c, 0);
+})
--- utils.js
+++ utils.js
@@ -1,9 +1,26 @@
 // @ts-check
-module.exports = {
+
+function timer (func) {
+    const start = performance.now()
+    const result = func();
+    const end = performance.now();
+    return { result, time: end - start };
+}
+const u = {
     /** @param {Function} func */
-    timer: (func) => {
-        console.time("advent");
-        console.log(func());
-        console.timeEnd("advent");
+    time: (func, its = 10) => {
+        const results = []
+        for (let i = 0; i < its; i++) results.push(timer(func))
+        const average = results.reduce((p, c) => p + c.time, 0) / results.length
+        console.log(`Results:\n${u.unique(results.map(r => r.result))}`)
+        console.log(`Average: ${average}ms`)
+        console.log(`Lowest: ${Math.min(...results.map(r => r.time))}ms`)
+        console.log(`Highest: ${Math.max(...results.map(r => r.time))}ms`)
+    },
+    /** @param {Array} arr */
+    unique: (arr) => {
+        return [...new Set(arr)]
     }
 }
+
+module.exports = u;
